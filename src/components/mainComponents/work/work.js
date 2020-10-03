8c0fe0f8de0dfbe46d3169bb74f015218565b167
Anthon Wellsjö <anthonwellsjo@gmail.com>
2020-10-03 12:00:40 +0200

    with less thumb rotations

--- src/components/mainComponents/work/work.js
+++ src/components/mainComponents/work/work.js
@@ -7,7 +7,7 @@ import Row from '../../layout/row/row';
 const Work = props => {
     const work = [
         { title: "Photo Model", duration: "2009-Ongoing", city: "Paris & Rome" },
-        { title: "Music Producer", duration: "2010-20015", city: "Paris" },
+        { title: "Music Producer", duration: "2010-2015", city: "Paris" },
         { title: "7-Eleven Staff", duration: "2008-2009", city: "Gothenburg" },
         { title: "Deconstruction Worker", duration: "2007-2008", city: "Gothenburg" },
     ]
