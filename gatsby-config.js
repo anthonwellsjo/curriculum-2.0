f7d92b510ce930d5a56a08430f29a332fcf378f1
Anthon Wellsjö <anthonwellsjo@gmail.com>
2020-10-28 22:34:37 +0100

    new landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
           `Kanit`,
           `Epilogue`,
           `Maven Pro`,
+          `Playfair Display`,
           `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
         ],
         display: 'swap'
--- src/components/mainComponents/profile/profile.js
+++ src/components/mainComponents/profile/profile.js
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import photo from '../../../images/portrait.jpg';
+import { useSpring, animated } from 'react-spring';
 import classes from './profile.module.css';
 import Centralizer from '../../layout/centralizer/centralizer';
 import Row from '../../layout/row/row';
@@ -9,26 +10,35 @@ import InnerTextHolder from '../../layout/innerTextHolder/innerTextHolder';
 import classnames from 'classnames';
 
 
-const Profile = props => {
-    return (
-        <Row reverse>
-            <Col>
-                <Centralizer column>
-                    <header className="profileHeader">
-                        <h1 className="profileHeaderText">Anthon Wellsjö</h1>
-                    </header>
-                    <InnerTextHolder className="hideOnHorizontal">
-                        <p className="presentationText">Swedish Web Developer based in Rome</p>
-                    </InnerTextHolder>
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
-                </Centralizer>
-            </Col>
-            <Col>
+
+const Profile = props => {
+    const [anim, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 1, tension: 3500, friction: 40 } }));
+
+    return (
+        <Centralizer column>
+            <div className={classes.photoHolder}>
                 <Centralizer column>
                     <img className={classnames(classes.img, "profilePic")} src={photo} alt="portrait" />
                 </Centralizer>
-            </Col>
-        </Row>
+            </div>
+            <div className={classes.frostedGlass}>
+                <Centralizer column>
+                    <Row reverse>
+                        <InnerTextHolder>
+                            <p className={classes.presentationText}>
+                                Hi, 
+                                <br></br>
+                                I'm <strong>Anthon Wellsjö</strong> and I'm a full stack web developer from Sweden.
+                                </p>
+                            </InnerTextHolder>
+                    </Row>
+                </Centralizer>
+            </div>
+        </Centralizer >
+
     )
 }
 
